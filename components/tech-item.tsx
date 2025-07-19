0b5b153b1d5009b99da273e42b719527aa7bb9eb
James Roberts <105246977+jlroberts1@users.noreply.github.com>
2025-07-18 21:37:45 -0400

    Remove unneeded components

--- components/tech-item.tsx
+++
@@ -1,9 +0,0 @@
-import { TechItemProps } from "@/app/types/tech-item"
-export function TechItem({ name, image }: TechItemProps) {
-  return (
-    <div className="flex flex-col items-center p-4">
-      <img src={image} alt={name} className="w-12 h-12 mb-2 text-primary" />
-      <span className="text-sm font-medium">{name}</span>
-    </div>
-  );
-}
